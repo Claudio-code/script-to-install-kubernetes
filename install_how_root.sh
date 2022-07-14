084f3a182c12db470402222b82d3d3d590c8bd50
claudio-code” <suporte2424@outlook.com>
2022-07-13 23:58:58 -0300

    added commands to make fields

--- install_how_root.sh
+++ install_how_root.sh
@@ -29,6 +29,9 @@ install_kubernetes () {
 start_kubernetes () {
     kubeadm config images pull
     kubeadm init
+    mkdir -p $HOME/.kube
+    cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+    chown $(id -u):$(id -g) $HOME/.kube/config
     kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 }
 
